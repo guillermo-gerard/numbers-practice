2127840b3756e147aa073f620d3fd33afcefe84c
Guillermo Gerard <guilloag@gmail.com>
2020-08-22 13:58:46 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/number_trainer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.number_trainer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/I18n/my_localizations.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MyLocalizations {
+  MyLocalizations(this.locale);
+
+  final Locale locale;
+
+  static MyLocalizations of(BuildContext context) {
+    return Localizations.of<MyLocalizations>(context, MyLocalizations);
+  }
+
+  static Map<String, Map<String, String>> _localizedValues = {
+    'en': {
+      'title': 'Numbers',
+    },
+    'es': {
+      'title': 'Números',
+    },
+  };
+
+  String get title {
+    return _localizedValues[locale.languageCode]['title'];
+  }
+}
+
+class MyLocalizationsDelegate extends LocalizationsDelegate<MyLocalizations> {
+  const MyLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'es'].contains(locale.languageCode);
+
+  @override
+  Future<MyLocalizations> load(Locale locale) {
+    return SynchronousFuture<MyLocalizations>(MyLocalizations(locale));
+  }
+
+  @override
+  bool shouldReload(MyLocalizationsDelegate old) => false;
+}
